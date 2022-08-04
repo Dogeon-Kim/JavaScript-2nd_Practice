e9496d224cacaafb6bb73c90addb8c583cf97d45
Dogeon-Kim <kdk5484@gmail.com>
2022-08-04 17:01:02 +0900

    First Commit.

---
+++ UP&DOWN/index.js
@@ -0,0 +1,24 @@
+var counter = 0;
+var randomNumber = ; //컴퓨터 1~100 무작위 수 생성
+
+document.getElementById("try") //키보드 클릭해 함수 실행
+
+if() //함수 실행 //kdypress 때 브라우저 기본 동작 : 취소
+
+function finding(){
+    var userNumber = document.getElementById("try").ariaValueMax;
+    if(){
+        if(randomNumber > userNumber){
+            document.getElementById("display").innerText = "UP!";
+        }
+        else if{
+
+        }
+        else{
+
+        }
+    }
+    else{
+
+    }
+}
